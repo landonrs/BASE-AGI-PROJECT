de25f78936b18b56e2b4aadf9de45db9a10e53b3
landonrs <altruist8@yahoo.com>
2018-08-17 16:21:15 -0600

    Fixed navmesh obstacles
    Neo was having issues going from the bedroom to the kitchen cause there
    was a slight gap between the walls, fixed this and adjusted mouse rotation
    sensitvity

--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
@@ -8,7 +8,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
     public class MouseLook
     {
         public float XSensitivity = 2f;
-        public float YSensitivity = 2f;
+        public float YSensitivity = 0f;
         public bool clampVerticalRotation = true;
         public float MinimumX = -90F;
         public float MaximumX = 90F;
